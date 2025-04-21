c1046bd31b8e78cb4d4c9a8f8ffda3f34b29b1cd
Naziur-Rahman <naziurr85@gmail.com>
2025-04-21 20:30:34 +0600

    Image Upload section update

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const ImageUpload = () => {
   return (
     <div className='bg-white shadow-lg rounded-2xl p-6 w-full max-w-2xl'>
-      <h1> Up Load your image </h1>
+     <label htmlFor='fileInput'  className='block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-y text-center hover:border-blue-500 transition-all'>
+        <input type='file' id='fileInput' className='hidden'></input>
+        <p className="p-2 text-lg font-medium text-gray-600"> Click or Drag and Drop  your image here</p>
+     </label>
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-lg text-gray-500'>
+    <div className='text-lg text-gray-500 mt-24'>
      
      Powered by @ Md.Naziur Rahman
     </div>
